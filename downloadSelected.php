3e43ffb1ea24f184d3ba813dd0253e755f215d10
Sharma Anshuman <mail@ashusharma.com>
2017-08-23 03:12:52 +0530

    Albums Download In Zip
    zip file download working mode

--- downloadSelected.php
+++ downloadSelected.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+set_time_limit(0);
 require_once './Facebook/autoload.php';
 $actionName = filter_input(INPUT_GET,"action");
 if($actionName == "Move Selected Albums!") {
@@ -12,27 +13,51 @@ if($actionName == "Move Selected Albums!") {
     //echo $uri;
     exit(0);
 }
+
         $fb = new Facebook\Facebook([
           'app_id' => $_SESSION['APPID'],
           'app_secret' => $_SESSION['APPSID'],
           'default_graph_version' => $_SESSION['VERID'],
           ]);
-        $accessToken = $_SESSION["fb"];        
-        $albumID = $_GET['c1'];
-        $folder = __DIR__;
-                $zip = new ZipArchive;
-                if ($zip->open($_SESSION['fbusernm'].'.zip', ZipArchive::OVERWRITE) === TRUE)  {
-                    for($i=0;$i<count($albumID);$i++)
-                    {
-                        $res = $fb->get('/'.$albumID[$i].'', $accessToken);
-                        $alnm =$res->getDecodedBody();
-                                $res = $fb->get('/'.$albumID[$i].'/photos?fields=picture,name,height,width,images,id', $accessToken);
-                                foreach($res->getDecodedBody()["data"] as $photo)
-                                {
-                                    $zip->addFromString($alnm['name']."/".$photo['id'].".jpg", file_get_contents($photo["images"][0]["source"]));
-                                }
-                    }
-                }
-                $zip->close();
 
-                header("location: ./".$_SESSION['fbusernm'].".zip");
+    $i=0; 
+    $accessToken = $_SESSION["fb"];        
+    $albumID = $_GET['c1'];
+    $folder = __DIR__;
+    $zip = new ZipArchive;
+    if ($zip->open($_SESSION['fbusernm'].'.zip', ZipArchive::OVERWRITE) === TRUE)  
+    {
+            for($i=0;$i<count($albumID);$i++)
+            {
+                $res = $fb->get('/'.$albumID[$i].'', $accessToken);
+                $alnm =$res->getDecodedBody();
+                do
+                {
+                    if($i==0)       
+                    {
+                        $res = $fb->get('/'.$albumID[$i].'/photos?fields=picture,name,height,width,images,id&limit=99999', $accessToken);
+                        foreach($res->getDecodedBody()["data"] as $photo)
+                        {
+                            $zip->addFromString($alnm['name']."/".$photo['id'].".jpg", file_get_contents($photo["images"][0]["source"]));
+                            $i++;
+                        }
+                    }
+                    else
+                    {
+                        $x=substr($res->getDecodedBody()["paging"]["next"],33);
+                        $res =  $fb->get($x, $accessToken);
+                        foreach($res->getDecodedBody()["data"] as $photo)
+                        {
+                            $zip->addFromString($alnm['name']."/".$photo['id'].".jpg", file_get_contents($photo["images"][0]["source"]));
+                            $i++;
+                        }       
+                    }
+                }while(isset($res->getDecodedBody()["paging"]["next"]));
+            }
+    }
+    $zip->close();
+		if(isset($_GET["click"])) {
+               echo "./".$_SESSION['fbusernm'].".zip";
+		} else {
+               header("location: ./".$_SESSION['fbusernm'].".zip");
+		}
